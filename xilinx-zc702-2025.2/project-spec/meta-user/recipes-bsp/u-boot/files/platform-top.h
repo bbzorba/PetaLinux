b21d007bc055a553490b6228d7a9144e1db7b4c8
bbzorba <bbzorba@hotmail.com>
2026-05-26 20:06:42 +0000

    booting correctly from u-boot

--- xilinx-zc702-2025.2/project-spec/meta-user/recipes-bsp/u-boot/files/platform-top.h
+++ xilinx-zc702-2025.2/project-spec/meta-user/recipes-bsp/u-boot/files/platform-top.h
@@ -14,3 +14,12 @@
 #if defined(CONFIG_ARCH_VERSAL_NET)
 #include <configs/xilinx_versal_net.h>
 #endif
+
+#undef CONFIG_BOOTCOMMAND
+
+#define CONFIG_BOOTCOMMAND \
+    "setenv autoload no; " \
+    "dhcp; " \
+    "tftpboot 0x2000000 Image; " \
+    "tftpboot 0x1f00000 system.dtb; " \
+    "bootm 0x2000000 - 0x1f00000"
